8281c7a1491d323d7b79ad696b90f9cd09e550c2
Kunal Saini <130821265+KunalSaini03@users.noreply.github.com>
2024-03-13 23:25:18 +0530

    first commit

---
+++ lib/data/database.dart
@@ -0,0 +1,21 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+  List toDoList = [];
+  final _myBox = Hive.box('mybox');
+
+  void createInitialData() {
+    toDoList = [
+      ["Create an App", false],
+      ["Do Exercise", false],
+    ];
+  }
+
+  void localData() {
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  void updateDataBase() {
+    _myBox.put("TODOLIST", toDoList);
+  }
+}
